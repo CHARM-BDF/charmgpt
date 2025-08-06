e36f6b0fab2a72751b4dcb8492d142dc5831f521
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2025-08-06 17:52:56 -0500

    Added system test that must be interactively run, but verifies if an plot artifact is returned from the service.

---
+++ system-tests/e2e/python-plot-sine.cy.ts
@@ -0,0 +1,45 @@
+describe('Charm-MCP Plotting a sine wave ', () => {
+    beforeEach( function() {
+    if (!Cypress.config('isInteractive')) {
+        this.skip();
+        }
+    });
+
+    it('should be able to interact with the chat input', () => {
+
+      cy.intercept('POST', 'http://localhost:5173/api/chat-artifacts').as('chatArtifacts');
+      cy.visit('/', { timeout: 30000 })
+
+      cy.get('button[title="Settings"]').click()
+      cy.get('button[title="Use Ollama"]').click()
+  
+      cy.get('button[title="Server Status"]').click()
+      cy.get('div').contains("Loading server status...").should('not.exist');
+      cy.get('[data-test="mcp-status-modal"]')
+        .find('[data-test="mcp-server-button"]')
+        .click({multiple: true})
+    
+      cy.get('[data-test="mcp-status-modal"]')
+        .contains('python')
+        .parent()
+        .find('[data-test="mcp-server-button"]')
+        .click()
+        
+      // Needs to force the button to click since it is not visible on the screen intentionally
+      cy.get('[data-test="mcp-status-modal"]')
+        .contains('button', 'Close')
+        .click({force: true})
+  
+      const testMessage = 'Plot a sine wave using my python mcp.'
+      
+      cy.get('textarea').first().should('be.visible').and('not.be.disabled')
+      
+      cy.get('textarea').first().type(testMessage).type('{enter}');
+      cy.get('span').contains("Assistant").should('exist')
+      cy.get('span').contains("Processing request...").should('exist')
+      cy.wait('@chatArtifacts', {responseTimeout: 1200000});
+      cy.get('span').contains("Using model provider: ollama").should('exist')
+      cy.get('span').contains("Response complete").should('exist');
+      cy.get('h3').contains("Python Generated PNG").should('exist');
+    })
+  }) 
